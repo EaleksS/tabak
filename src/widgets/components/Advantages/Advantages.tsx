7300edf1362d2a858884815631028040846adbaa
EaleksS <ealgenv@gmail.com>
2023-06-03 20:18:17 +0900

    icon

--- src/widgets/components/Advantages/Advantages.tsx
+++ src/widgets/components/Advantages/Advantages.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 import styles from "./Advantages.module.scss";
 import { Text } from "../../../shared";
+import {
+  FcBullish,
+  FcDiploma1,
+  FcCheckmark,
+  FcDeployment,
+} from "react-icons/fc";
 
 export const Advantages: FC = (): JSX.Element => {
   return (
@@ -14,8 +20,10 @@ export const Advantages: FC = (): JSX.Element => {
       </div>
       <div className={styles.items}>
         <div className={styles.item}>
-          <img src="/img/conveyor.svg" alt="conveyor" />
-          <Text type="h3">Производство и <br /> технологии</Text>
+          <FcDeployment className={styles.icon} />
+          <Text type="h3">
+            Производство и <br /> технологии
+          </Text>
           <Text type="p5">
             Собственная производственная линия и использование современных
             технологий гарантируют высокие стандарты качества наших табачных
@@ -23,7 +31,7 @@ export const Advantages: FC = (): JSX.Element => {
           </Text>
         </div>
         <div className={styles.item}>
-          <img src="/img/tobaccos.svg" alt="tobaccos" />
+          <FcCheckmark className={styles.icon} />
           <Text type="h3">
             Табак и <br /> добавки
           </Text>
@@ -34,8 +42,10 @@ export const Advantages: FC = (): JSX.Element => {
           </Text>
         </div>
         <div className={styles.item}>
-          <img src="/img/certificate.svg" alt="certificate" />
-          <Text type="h3">Легальность и <br /> маркировка</Text>
+          <FcDiploma1 className={styles.icon} />
+          <Text type="h3">
+            Легальность и <br /> маркировка
+          </Text>
           <Text type="p5">
             Создаём абсолютно легальный продукт с наличием сертификатов и
             декларации соответствия. На всю продукцию нанесены акцизные марки и
@@ -43,8 +53,10 @@ export const Advantages: FC = (): JSX.Element => {
           </Text>
         </div>
         <div className={styles.item}>
-          <img src="/img/rising.svg" alt="rising" />
-          <Text type="h3">Развитие и <br /> внимательность</Text>
+          <FcBullish className={styles.icon} />
+          <Text type="h3">
+            Развитие и <br /> внимательность
+          </Text>
           <Text type="p5">
             Постоянно развиваемся. Увеличиваем производство и разрабатываем
             новые линейки продукции. Всегда готовы ответить на вопросы и принять
